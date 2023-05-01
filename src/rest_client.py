bc7fc2b9ec627cc6fd821f2fe533d3f3560cea03
daniel868 <sincalexandrudaniel@gmail.com>
2023-05-01 21:10:15 +0300

    Add Medical analyses and VideoReading capabilities

--- src/rest_client.py
+++ src/rest_client.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, Response, url_for, redirect
 from kafka_service import KafkaService
+from AddMedicalPredict import AddMedicalPredict
 import requests
+import json
 
 app = Flask(__name__)
 is_authenticated = False
 kafka_service = KafkaService()
+medical_predict_service = AddMedicalPredict()
 
 
 @app.route('/failed', methods=['GET'])
@@ -32,6 +35,7 @@ def video():
         mimetype='multipart/x-mixed-replace; boundary=frame'
     )
 
+
 @app.route('/video-png', methods=['GET'])
 def video_png():
     global is_authenticated
@@ -52,6 +56,15 @@ def video_png():
         mimetype='multipart/x-mixed-replace; boundary=frame'
     )
 
+
+@app.route('/read-video', methods=['GET'])
+def read_video():
+    return Response(
+        kafka_service.read_file_stream(),
+        mimetype='multipart/x-mixed-replace; boundary=frame'
+    )
+
+
 def isUserAuthenticated():
     response = requests.post(
         'http://localhost:8080/api/v1/user/checkValidAuthentication',
@@ -59,6 +72,25 @@ def isUserAuthenticated():
         headers={"Authorization": f"Bearer {kafka_service.user_token}"}
     )
     return response.status_code == 200
+
+
+@app.route('/medical-info', methods=['GET'])
+def medical_information():
+    json_response = json.dumps(medical_predict_service.med_prediction).encode("utf-8")
+    return Response(
+        json_response,
+        mimetype="application/json"
+    )
+
+
+@app.route('/video-fps', methods=['GET'])
+def get_video_fps():
+    response = {'FPS': round(kafka_service.fps, 2)}
+    json_response = json.dumps(response).encode("utf-8")
+    return Response(
+        json_response,
+        mimetype="application/json"
+    )
 
 
 if __name__ == "__main__":
